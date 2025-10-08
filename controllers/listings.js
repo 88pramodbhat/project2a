181279f4e8ce7e1bd690c9bbf6fe0fdc5dcc2e27
pramod bhat <pramodbhat396@gmail.com>
2025-10-08 22:19:16 +0530

    Add all project files

--- controllers/listings.js
+++
@@ -1,6 +0,0 @@
-const Listing = require("../model/listing");
-
-module.exports.index = async (req, res) => {
-  const all_listings = await Listing.find({});
-  res.render("listings/index.ejs", { all_listings });
-};
